873019f4006dea455d34f4889b4770a58b5e4c9b
Zhukovec Artur <bnlvdn7483@gmail.com>
2022-07-20 03:23:30 +0300

    [upd 3] Working with styles. Working with the logic of the first application

--- web_assistant/shorter/urls.py
+++ web_assistant/shorter/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import shorter
+from .views import shorter, to_source_url
 
 urlpatterns = [
-    path('', shorter, name='shorter')
+    path('', shorter, name='shorter'),
+    path('<str:url_id>/', to_source_url, name='to_source_url')
 ]
--- web_assistant/shorter/views.py
+++ web_assistant/shorter/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from uuid import uuid4
+from .forms import UrlForm
+from .models import Url
+
+
+DOMAIN = 'localhost:8000/'  # insert server address here.
 
 
 def shorter(request):
-    return render(request, 'shorter/shorter.html')
+    list_of_urls = Url.objects.all()
+    url_id = str(uuid4())[:6]
+    if request.method == 'POST':
+        form = UrlForm(request.POST)
+        if form.is_valid() and not Url.objects.filter(source_url=form.cleaned_data['source_url']):
+            new_url = Url(source_url=form.cleaned_data['source_url'], url_id=url_id)
+            new_url.save()
+            generated_url = DOMAIN + url_id
+            context = {'form': form, 'url_id': url_id, 'result':generated_url, 'list':list_of_urls}
+            return render(request, 'shorter/shorter.html', context)
+
+        else:
+            error = 'This url is already exist'
+            return render(request, 'shorter/shorter.html', {'form': form, 'error':error, 'list':list_of_urls})
+    else:
+        form = UrlForm()
+    return render(request, 'shorter/shorter.html', context={'form': form, 'list':list_of_urls})
+
+
+def to_source_url(request, url_id):
+    """
+     If the user clicks on the shortened URL,
+     the function matches it with the source and redirects to the source address
+    """
+    origin_url = Url.objects.get(url_id=url_id)
+    return redirect(origin_url.source_url)
+
+
+
